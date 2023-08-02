9b59e933f50ef5ec0da68baef679053672874937
Eemeli Aro <eemeli@mozilla.com>
2023-08-02 15:22:19 +0300

    Add MF2-ish message/resource representations (#19)

---
+++ compare_locales/resource/from_properties.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import re
+from typing import Iterable, List, Optional, Union
+
+from ..parsers import Comment as ParserComment
+from ..parsers import Entity as ParserEntity
+from ..parsers import Junk as ParserJunk
+from ..parsers import Whitespace as ParserWhitespace
+from .elements import (
+    Comment,
+    Entry,
+    Junk,
+    Message,
+    Pattern,
+    PatternMessage,
+    Text,
+    VariableRef,
+)
+
+
+def patternFromPropertiesValue(
+    value: str, variables: Optional[re.Pattern[str]] = None
+) -> PatternMessage:
+    """
+    Compile a .properties value into a PatternMessage data class.
+
+    `variables` may be a compiled regexp identifying a variable placeholder from the value.
+    Its captured groups (if any) are ignored.
+    """
+
+    pattern: Pattern = []
+    prev_end = 0
+    if variables:
+        for match in variables.finditer(value):
+            start = match.start()
+            if start > prev_end:
+                pattern.append(Text(value[prev_end:start]))
+            pattern.append(VariableRef(match[0]))
+            prev_end = match.end()
+    if len(pattern) == 0 or prev_end < len(value):
+        pattern.append(Text(value[prev_end:]))
+    return PatternMessage(pattern)
+
+
+def resourceFromProperties(
+    entries: Iterable[Union[ParserComment, ParserWhitespace, ParserEntity, ParserJunk]],
+    variables: Optional[re.Pattern[str]] = None,
+) -> List[Entry]:
+    """
+    Compile a parsed .properties file into a message resource.
+
+    `variables` may be a compiled regexp identifying a variable placeholder.
+    """
+
+    res: List[Entry] = []
+    for pe in entries:
+        if isinstance(pe, ParserEntity):
+            value = patternFromPropertiesValue(pe.val, variables)
+            cspan = pe.pre_comment.span if pe.pre_comment else None
+            res.append(Message((pe.key,), value, pe.span, cspan))
+        elif isinstance(pe, ParserComment):
+            res.append(Comment(pe.span))
+        elif not isinstance(pe, ParserWhitespace):
+            res.append(Junk(pe.span))
+    return res
---
+++ compare_locales/resource/tests/test_fluent.py
@@ -0,0 +1,227 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import unittest
+
+from fluent.syntax import FluentParser
+
+from .. import (
+    CatchallKey,
+    Comment,
+    FunctionRef,
+    Junk,
+    Literal,
+    Message,
+    PatternMessage,
+    SelectMessage,
+    Text,
+    VariableRef,
+    resourceFromFluent,
+)
+
+
+class TestFluentParser(unittest.TestCase):
+    def test_simple_message(self):
+        src = "a = A"
+        ast = FluentParser().parse(src)
+        res = resourceFromFluent(ast)
+        self.assertEqual(
+            res,
+            [Message(("a",), PatternMessage([Text("A")]), (0, 5))],
+        )
+
+    def test_complex_message(self):
+        src = "abc = A { $arg } B { msg } C"
+        ast = FluentParser().parse(src)
+        res = resourceFromFluent(ast)
+        self.assertEqual(
+            res,
+            [
+                Message(
+                    ("abc",),
+                    PatternMessage(
+                        [
+                            Text("A "),
+                            VariableRef("arg"),
+                            Text(" B "),
+                            FunctionRef("MESSAGE", Literal(False, "msg")),
+                            Text(" C"),
+                        ]
+                    ),
+                    res[0].span,
+                )
+            ],
+        )
+
+    def test_multiline_message(self):
+        src = """\
+abc =
+    A
+    B
+    C
+"""
+        ast = FluentParser().parse(src)
+        res = resourceFromFluent(ast)
+        self.assertEqual(
+            res,
+            [Message(("abc",), PatternMessage([Text("A\nB\nC")]), res[0].span)],
+        )
+
+    def test_message_with_attribute(self):
+        src = """\
+
+
+abc = ABC
+    .attr = Attr
+"""
+        ast = FluentParser().parse(src)
+        res = resourceFromFluent(ast)
+        self.assertEqual(
+            res,
+            [
+                Message(("abc",), PatternMessage([Text("ABC")]), (2, 11)),
+                Message(("abc", "attr"), PatternMessage([Text("Attr")]), (16, 28)),
+            ],
+        )
+
+    def test_message_with_attribute_and_no_value(self):
+        src = """\
+abc =
+    .attr = Attr
+"""
+        ast = FluentParser().parse(src)
+        res = resourceFromFluent(ast)
+        self.assertEqual(
+            res,
+            [
+                Message(("abc", "attr"), PatternMessage([Text("Attr")]), (10, 22)),
+            ],
+        )
+
+    def test_non_localizable(self):
+        src = """\
+### Resource Comment
+
+foo = Foo
+
+## Group Comment
+
+-bar = Bar
+
+##
+
+# Standalone Comment
+
+# Baz Comment
+baz = Baz
+"""
+        ast = FluentParser().parse(src)
+        res = resourceFromFluent(ast)
+        self.assertEqual(
+            res,
+            [
+                Comment(res[0].span),
+                Message(("foo",), PatternMessage([Text("Foo")]), res[1].span),
+                Comment(res[2].span),
+                Message(("-bar",), PatternMessage([Text("Bar")]), res[3].span),
+                Comment(res[4].span),
+                Comment(res[5].span),
+                Message(
+                    ("baz",),
+                    PatternMessage([Text("Baz")]),
+                    res[6].span,
+                    comment=res[6].comment,
+                ),
+            ],
+        )
+        c = res[0].span
+        self.assertEqual(src[c[0] : c[1]], "### Resource Comment")
+        c = res[2].span
+        self.assertEqual(src[c[0] : c[1]], "## Group Comment")
+        c = res[4].span
+        self.assertEqual(src[c[0] : c[1]], "##")
+        c = res[5].span
+        self.assertEqual(src[c[0] : c[1]], "# Standalone Comment")
+        c = res[6].comment
+        self.assertEqual(src[c[0] : c[1]], "# Baz Comment")
+
+    def test_junk(self):
+        src = """\
+# Comment
+
+Line of junk
+
+# Comment
+msg = value
+"""
+        ast = FluentParser().parse(src)
+        res = resourceFromFluent(ast)
+        self.assertEqual(
+            res,
+            [
+                Comment(res[0].span),
+                Junk(res[1].span),
+                Message(
+                    ("msg",),
+                    PatternMessage([Text("value")]),
+                    res[2].span,
+                    comment=res[2].comment,
+                ),
+            ],
+        )
+        c = res[0].span
+        self.assertEqual(src[c[0] : c[1]], "# Comment")
+        c = res[1].span
+        self.assertEqual(src[c[0] : c[1]], "Line of junk\n\n")
+        c = res[2].comment
+        self.assertEqual(src[c[0] : c[1]], "# Comment")
+
+    def test_multiple_selectors(self):
+        src = """
+abc =
+  { $a ->
+     [one]
+       { $b ->
+          [two] one-two
+         *[other] one-other
+       }
+    *[other]
+       { $b ->
+          [two] other-two
+         *[other] other-other
+       }
+  }
+"""
+        ast = FluentParser().parse(src)
+        res = resourceFromFluent(ast)
+        self.assertEqual(
+            res,
+            [
+                Message(
+                    ("abc",),
+                    SelectMessage(
+                        selectors=[VariableRef("a"), VariableRef("b")],
+                        variants=[
+                            (
+                                [Literal(False, "one"), Literal(False, "two")],
+                                [Text("one-two")],
+                            ),
+                            (
+                                [Literal(False, "one"), CatchallKey("other")],
+                                [Text("one-other")],
+                            ),
+                            (
+                                [CatchallKey("other"), Literal(False, "two")],
+                                [Text("other-two")],
+                            ),
+                            (
+                                [CatchallKey("other"), CatchallKey("other")],
+                                [Text("other-other")],
+                            ),
+                        ],
+                    ),
+                    res[0].span,
+                )
+            ],
+        )
